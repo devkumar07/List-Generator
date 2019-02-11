020cd759fcdea2e76dffe9fcb74dd7f74240da4c
Devanshu Kumar <devanshu.kumar07@gmail.com>
2019-02-10 19:06:18 -0800

    fixed the text generation issue

--- Project_data.java
+++ Project_data.java
@@ -3,11 +3,17 @@ public class Project_data{
   private String number;
   private String organization;
   private String title;
-  public Project_data(String title, String number, String project_id, String organization){
+  public Project_data(String project_id, String number, String organization, String title){
     this.project_id=project_id;
     this.number=number;
     this.organization=organization;
     this.title=title;
+  }
+  public Project_data(Project_data list){
+    this.project_id=list.project_id;
+    this.number=list.number;
+    this.organization=list.organization;
+    this.title=list.title;
   }
   public String get_title(){
     return title;
--- Team_agreement.java
+++ Team_agreement.java
@@ -8,38 +8,15 @@ import java.util.StringTokenizer;
 import java.util.Scanner;
 import java.io.PrintWriter;
 public class Team_agreement{
-  private Project_data[] project;
-  private Student_data[] student;
-  //This method is used for testing purposes to see if the objects were storing the appropriate data.
-  public String get_candidates(int a){
-      return project[a].get_title()+" "+student[a].get_first_name()+" "+student[a].get_last_name()+" "+student[a].get_Team()+" "+student[a].get_Time();
-  }
-
-  public void set_student_roster(Student_data[] list){
-    student=new Student_data[list.length];
+  public Project_data Project;
+  public Student_data[] Student;
+  public Team_agreement(Student_data student_roster[], Project_data project_roster){
+    Project=new Project_data(project_roster);
+    Student=new Student_data[student_roster.length];
     int i=0;
-    while(list[i]!=null){
-      student[i]=new Student_data(list[i].get_Team(),list[i].get_first_name(),list[i].get_last_name(),list[i].get_email_id(),list[i].get_agree(),list[i].get_Time());
+    while(student_roster[i]!=null){
+      Student[i]=new Student_data(student_roster[i]);
       i++;
-    }
-  }
-  public void set_project_roster(Project_data[] list){
-    project=new Project_data[list.length];
-    int i=0;
-    while(list[i]!=null){
-      project[i]=new Project_data(list[i].get_title(),list[i].get_number(),list[i].get_project_id(),list[i].get_organization());
-      i++;
-    }
-  }
-  public void output_roster(int index){
-    String fileName = "test"+index+".txt";
-    try{
-      PrintWriter outputStream = new PrintWriter(fileName);
-       outputStream.println();
-       outputStream.close();
-    }
-    catch(FileNotFoundException e){
-      e.printStackTrace();
     }
   }
 }
