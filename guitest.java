3fd01b87e033bba9b43a3f290964e4473557d293
kiran <kiran@kiranraj.us>
2019-02-22 21:31:47 -0800

    Test JFrame

---
+++ guitest.java
@@ -0,0 +1,10 @@
+import java.awt.GraphicsConfiguration;
+import javax.swing.JFrame;
+
+public class guitest {
+    public static void main(String[] args) {
+      JFrame frame= new JFrame("test");
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		  frame.setVisible(true);
+  }
+}
